9f8ad9febf32891ba90d5d69f41890f21156f308
c22moon <ansrjsdn7@naver.com>
2018-02-08 11:12:08 +0900

    코멘트 보기 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id','author','title','published_date')
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id','author','text','approved_comment')
+
 # Register your models here.
 admin.site.register(Post,PostAdmin)
+admin.site.register(Comment,CommentAdmin)
--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from blog.models import Post
+from blog.models import Post, Comment
 
 # class PostForm(forms.Form):
 #     title = forms.CharField(max_length=200)
@@ -10,3 +10,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title','text')
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author','text',)
